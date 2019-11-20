ef1394736cf60515682e4e9eb8a840dd029ec12b
david <davidstephane31@gmail.com>
2019-11-20 15:04:17 +0100

    compte test

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -6,6 +6,7 @@ const HomePage = (props) => {
             <h1 className="display-3">Bienvenue</h1>
             <p className="lead">FluidCrm est une application de gestion de clients dévelopée à l'aide de Symfony 4, API Platform et React js</p>
             <hr className="my-4" />
+            <p>Créer un compte ou connectez vous avec les identifiants de test: <br/>Adresse email: ericd@gmail.com <br/>Mot de passe: passtest</p>
             <p>Une interface fluide et dynamique vous attends pour la gestion de vos clients :)</p>
             <p className="lead">
                 <a className="btn btn-primary btn-lg" href="#" role="button">Découvrir</a>
